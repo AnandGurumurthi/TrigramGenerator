811850bdb2e785ef651d48a91812d385d8d8522f
Anand Gurumurthi <anand.gurumurthi@gmail.com>
2011-10-28 11:28:15 -0700

    Moving the src folders inside src/main and adding test folder

---
+++ src/main/java/com/trigram/TrigramGeneratorServlet.java
@@ -0,0 +1,13 @@
+package com.trigram;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TrigramGeneratorServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");		
+		Trigram trigram = new Trigram();
+		resp.getWriter().println(trigram.generateTrigram("I wish I may I wish I might"));
+	}
+}

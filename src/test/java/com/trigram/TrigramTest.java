cb46de94be28469729b40146d9e09f1f4efdae9c
AnandGurumurthi <anand.gurumurthi@gmail.com>
2011-10-28 23:25:21 -0700

    Added test cases. Also added testng jar

--- src/test/java/com/trigram/TrigramTest.java
+++ src/test/java/com/trigram/TrigramTest.java
@@ -1,8 +1,43 @@
 package com.trigram;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Test class to test Trigram class
+ * 
+ * @author Anand
+ *
+ */
 public class TrigramTest {
 
-	public void testGenerateTrigram() {
-		//TODO Add test code here
+	/**
+	 * Positive test case. Tests the program with a valid input
+	 */
+	@Test
+	public void testWithValidInput() {
+		Trigram trigram = new Trigram();
+		String generatedTrigram = trigram.generateTrigram("I wish I may I wish I might");
+		Assert.assertNotNull(generatedTrigram);
+	}
+	
+	/**
+	 * Negative test case. Tests the program with a null input
+	 */
+	@Test
+	public void testWithNull() {
+		Trigram trigram = new Trigram();
+		String generatedTrigram = trigram.generateTrigram(null);
+		Assert.assertEquals(generatedTrigram, "");
+	}
+	
+	/**
+	 * Negative test case. Tests the program with an empty input
+	 */
+	@Test
+	public void testWithEmptyInput() {
+		Trigram trigram = new Trigram();
+		String generatedTrigram = trigram.generateTrigram("");
+		Assert.assertEquals(generatedTrigram, "");
 	}
 }
